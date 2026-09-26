c3d8f4a998bbabb99e0a8f945027832decbdf838
ClipboardWorks <gelbet.market@gmail.com>
2026-09-26 14:57:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const VERSION='up-demo-1.0.2';
+const VERSION='up-demo-1.0.3';
 const FILES=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(VERSION).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k.startsWith('up-demo-')&&k!==VERSION).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;
   e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(VERSION).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('index.html'))))});
